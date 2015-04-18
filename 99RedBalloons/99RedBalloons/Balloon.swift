f0dc3aa664c9365c7c5d9fd48a2f09f568ca88d9
chicovg <guthrievictor@gmail.com>
2015-04-18 15:34:15 -0400

    completed 99 red balloons challenge

---
+++ 99RedBalloons/99RedBalloons/Balloon.swift
@@ -0,0 +1,16 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Victor Guthrie on 4/18/15.
+//  Copyright (c) 2015 chicovg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var number = 0;
+    var image = UIImage(named:"")
+    
+}
---
+++ 99RedBalloons/99RedBalloons/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  99RedBalloons
+//
+//  Created by Victor Guthrie on 4/18/15.
+//  Copyright (c) 2015 chicovg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var balloonLabel: UILabel!
+    @IBOutlet weak var balloonImage: UIImageView!
+    
+    var currentBallon:Int = 0
+    var ballons:[Balloon] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createBalloons()
+        setBalloon()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func createBalloons(){
+        for n in 1...99 {
+            var balloon = Balloon()
+            balloon.number = n
+            balloon.image = randomImage()
+            
+            ballons.append(balloon)
+        }
+    }
+    
+    func setBalloon() {
+        var balloonToSet = ballons[currentBallon];
+        balloonLabel.text = "\(balloonToSet.number) balloon"
+        balloonImage.image = balloonToSet.image
+    }
+    
+    func randomImage() -> UIImage {
+        let index = Int(arc4random_uniform(UInt32(4)));
+        var imageName: String
+        
+        switch index {
+            case 0 : imageName = "RedBalloon1.jpg"
+            case 1 : imageName = "RedBalloon2.jpg"
+            case 2 : imageName = "RedBalloon3.jpg"
+            default : imageName = "RedBalloon4.jpg"
+        }
+        return UIImage(named: imageName)!
+    }
+
+    @IBAction func nextButtonPressed(sender: AnyObject) {
+        currentBallon++
+        setBalloon()
+    }
+
+}
+
